ff154611015e85777fc7e4b9c7095d453a0db8ba
Marco Molteni <marco.molteni@mailbox.org>
2023-06-18 15:22:45 +0200

    refactor: move commands to new file commands.go

--- procedure.go
+++ procedure.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"os"
 	"strings"
 
 	"github.com/alecthomas/kong"
@@ -193,27 +192,4 @@ func printToc(pcd *Procedure) {
 			strings.TrimSpace(step.Title))
 	}
 	fmt.Println()
-}
-
-func cmdNext(pcd *Procedure) error {
-	if pcd.stepIdx >= len(pcd.steps) {
-		return fmt.Errorf("next: internal error: step index > len(steps)")
-	}
-	step := pcd.steps[pcd.stepIdx]
-	fmt.Printf("\n## (%d) %s\n\n", pcd.stepIdx+1, strings.TrimSpace(step.Title))
-
-	if step.Desc != "" {
-		if err := render(os.Stdout, strings.TrimSpace(step.Desc),
-			pcd.bag); err != nil {
-			return fmt.Errorf("%s %w", err, ErrUnrecoverable)
-		}
-		fmt.Printf("\n\n")
-	}
-
-	if err := step.Run(pcd.bag); err != nil {
-		return fmt.Errorf("step %d: %w", pcd.stepIdx+1, err)
-	}
-	pcd.stepIdx++
-
-	return nil
 }
